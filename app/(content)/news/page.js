2b43109644535e9a6377075cc2a2e4e4f49179ef
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-09-08 17:00:42 +0530

    Make fetching data in NextJs more easier

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,43 +1,20 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import NewsList from "@/components/news-list";
 
-const NewsPage = () => {
-  const [news, setNews] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("http://localhost:8080/news");
-        const newsData = await response.json();
-        setNews(newsData);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+  const newsData = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {news && news.length > 0 && <NewsList news={news} />}
+      <NewsList news={newsData} />
     </>
   );
 };
